07c7c5aea6c3601d1e9b302ac09cb940abc90f49
elizabete silva <elizabete.airam@gmail.com>
2022-02-27 13:50:39 -0300

    endpoints shuffle cards methods

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const app = express()
-const importData = require('./data.json')
+const importData = require("./data.json")
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-  res.send("Cartas do tarot rider waite smith")
+  res.send("* API Cartas do tarot Rider-Waite Smith *")
 })
 
 app.get("/cartas", (req, res) => {
@@ -16,31 +16,74 @@ app.get("/cartas/id/:id", (req, res) => {
   res.send(importData.cartas[id])
 })
 
+app.get("/cartas/jogo/sortear/:sortear", (req, res) => {
+  const { sortear }  = req.params
+  const embaralhar = importData.cartas.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, sortear)
+  res.send(cartas_sorteadas)
+})
+
+app.get("/cartas/jogo/sortear/5/cruz-verdade", (req, res) => {
+  const embaralhar = importData.cartas.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, 5)
+  res.send(cartas_sorteadas)
+})
+
+app.get("/cartas/jogo/sortear/9/nove-cartas", (req, res) => {
+  const embaralhar = importData.cartas.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, 9)
+  res.send(cartas_sorteadas)
+})
+
+app.get("/cartas/jogo/sortear/10/cruz-celta", (req, res) => {
+  const embaralhar = importData.cartas.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, 10)
+  res.send(cartas_sorteadas)
+})
+
+app.get("/cartas/jogo/sortear/7/ancora", (req, res) => {
+  const arcano_maior = importData.cartas.slice(0, 22)
+  const embaralhar = arcano_maior.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, 7)
+  res.send(cartas_sorteadas)
+})
+
+app.get("/cartas/jogo/sortear/21/vida", (req, res) => {
+  const embaralhar = importData.cartas.sort(() => Math.random() - 0.5)
+  const cartas_sorteadas = embaralhar.slice(0, 21)
+  res.send(cartas_sorteadas)
+})
+
 app.get("/cartas/tipo/arcano-maior", (req, res) => {
-  const arcano_maior = importData.cartas.slice(0,22)
+  const arcano_maior = importData.cartas.slice(0, 22)
   res.send(arcano_maior)
 })
 
+app.get("/cartas/tipo/arcano-menor", (req, res) => {
+  const arcano_menor = importData.cartas.slice(22, 79)
+  res.send(arcano_menor)
+})
+
 app.get("/cartas/valor/paus", (req, res) => {
-  const pausData = importData.cartas.slice(22,36)
+  const pausData = importData.cartas.slice(22, 36)
   res.send(pausData)
 })
 
 app.get("/cartas/valor/copas", (req, res) => {
-  const copasData = importData.cartas.slice(36,50)
+  const copasData = importData.cartas.slice(36, 50)
   res.send(copasData)
 })
 
 app.get("/cartas/valor/espadas", (req, res) => {
-  const espadasData = importData.cartas.slice(50,65)
+  const espadasData = importData.cartas.slice(50, 65)
   res.send(espadasData)
 })
 
 app.get("/cartas/valor/ouros", (req, res) => {
-  const ourosData = importData.cartas.slice(65,79)
+  const ourosData = importData.cartas.slice(65, 79)
   res.send(ourosData)
 })
 
 app.listen(port, () => {
-   console.log(`Servidor na porta http://localhost:${port} o/`)
+   console.log(`Servidor aberto na porta http://localhost:${port} o/`)
 })
